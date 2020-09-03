9fb9c9b6db50bebcd2be014d0b721adc30c20e44
letsandeepio <sandeepchopra7@gmail.com>
2020-09-02 20:17:10 -0400

    implements grid system for moview views

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,18 +6,19 @@ import { useStateValue } from '../context/StateProvider';
 const Card = ({ movie }) => {
   const [{ nominatedMovies, isNominationFull }, dispatch] = useStateValue();
   return (
-    <div className="card">
-      <img
-        src={
+    <div
+      className="card"
+      style={{
+        background: `url(${
           movie.Poster === 'N/A'
             ? 'https://dummyimage.com/150x200/efefef/000000.png&text=Poster+not+available'
             : movie.Poster
-        }
-        alt=""
-      ></img>
-      <h6>
-        {movie.Title}({movie.Year})
-      </h6>
+        })`,
+        backgroundSize: 'cover'
+      }}
+    >
+      <div className="card__title">{movie.Title}</div>
+      <div className="card__year">{movie.Year}</div>
       <button
         onClick={() => dispatch({ type: 'ADD_NOMINATION', id: movie.imdbID })}
         disabled={nominatedMovies.includes(movie.imdbID) || isNominationFull}
