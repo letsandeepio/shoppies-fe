5f1523f189c79210e6add0233e6f05b1780fa96b
letsandeepio <sandeepchopra7@gmail.com>
2020-09-02 21:52:45 -0400

    styles the movie cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,30 +2,40 @@ import React from 'react';
 import './Card.css';
 
 import { useStateValue } from '../context/StateProvider';
+import { TrophyOutlined } from '@ant-design/icons';
 
 const Card = ({ movie }) => {
   const [{ nominatedMovies, isNominationFull }, dispatch] = useStateValue();
   return (
-    <div
-      className="card"
-      style={{
-        background: `url(${
-          movie.Poster === 'N/A'
-            ? 'https://dummyimage.com/150x200/efefef/000000.png&text=Poster+not+available'
-            : movie.Poster
-        })`,
-        backgroundSize: 'cover'
-      }}
-    >
-      <div className="card__title">{movie.Title}</div>
-      <div className="card__year">{movie.Year}</div>
-      <button
-        onClick={() => dispatch({ type: 'ADD_NOMINATION', id: movie.imdbID })}
-        disabled={nominatedMovies.includes(movie.imdbID) || isNominationFull}
+    <div className="card">
+      <div
+        className="card__content"
+        style={{
+          background: `url(${
+            movie.Poster === 'N/A'
+              ? 'https://dummyimage.com/150x200/efefef/000000.png&text=Poster+not+available'
+              : movie.Poster
+          })`,
+          backgroundSize: 'cover'
+        }}
       >
-        Nominate
-      </button>
-      {nominatedMovies.includes(movie.imdbID) && 'Nominate'}
+        <div className="card__action">
+          <button
+            onClick={() =>
+              dispatch({ type: 'ADD_NOMINATION', id: movie.imdbID })
+            }
+            disabled={
+              nominatedMovies.includes(movie.imdbID) || isNominationFull
+            }
+          >
+            <TrophyOutlined />
+          </button>
+        </div>
+        <div className="card__title">
+          {movie.Title}({movie.Year})
+        </div>
+        <div className="card__year"></div>
+      </div>
     </div>
   );
 };
