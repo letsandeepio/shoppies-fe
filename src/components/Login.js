0113dba14d592b58aa497ae784dcf3aa07666e39
letsandeepio <sandeepchopra7@gmail.com>
2020-09-03 18:09:29 -0400

    minor ui & bug fixes on login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,45 +3,58 @@ import Logo from './Logo';
 import './Login.css';
 
 const Login = () => {
-  const [{ login, name, email, password }, setState] = useState({
+  const [state, setState] = useState({
     login: '',
     name: '',
     email: '',
     password: ''
   });
+  const { login, name, email, password } = state;
   return (
-    <div className="login">
-      <div className="login__container">
-        <Logo />
-        <h4>{login ? 'Login' : 'Sign Up'}</h4>
-        {!login && (
+    <form>
+      <div className="login">
+        <div className="login__container">
+          <Logo />
+          <h4>{login ? 'Login' : 'Sign Up'}</h4>
+
           <input
             value={name}
-            onChange={(e) => setState({ name: e.target.value })}
+            onChange={(e) => setState({ ...state, name: e.target.value })}
             type="text"
             placeholder="Your name"
+            style={{ visibility: login ? 'hidden' : 'visible' }}
           />
-        )}
-        <input
-          value={email}
-          onChange={(e) => setState({ email: e.target.value })}
-          type="text"
-          placeholder="Your email address"
-        />
-        <input
-          value={password}
-          onChange={(e) => setState({ password: e.target.value })}
-          type="password"
-          placeholder="Choose a safe password"
-        />
-        <div
-          onClick={() => setState({ login: !login })}
-          className="login__options"
-        >
-          {login ? 'need to create an account?' : 'already have an account?'}
+
+          <input
+            value={email}
+            onChange={(e) => setState({ ...state, email: e.target.value })}
+            type="text"
+            placeholder="Your email address"
+          />
+          <input
+            value={password}
+            onChange={(e) => setState({ ...state, password: e.target.value })}
+            type="password"
+            placeholder="Choose a safe password"
+          />
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log('hello');
+            }}
+          >
+            {login ? 'login' : 'create account'}
+          </button>
+          <div
+            onClick={() => setState({ ...state, login: !login })}
+            className="login__options"
+          >
+            {login ? 'need to create an account?' : 'already have an account?'}
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
